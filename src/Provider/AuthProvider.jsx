6fa30568249670d9e101d56162635c86a5df83f3
MuhammadAtiq <muhammadatiq757@gamil.com>
2023-05-21 20:03:08 +0600

    private route done

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,54 +1,54 @@
-import { createContext, useEffect, useState } from "react";
-import app from "../Firebase/firebase.config";
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth"
+// import { createContext, useEffect, useState } from "react";
+// import app from "../Firebase/firebase.config";
+// import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth"
 
-export const AuthContext = createContext(null)
-const auth = getAuth(app)
-const AuthProvider = ({ children }) => {
+// export const AuthContext = createContext(null)
+// const auth = getAuth(app)
+// const AuthProvider = ({ children }) => {
 
-    const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(true)
+//     const [user, setUser] = useState(null);
+//     const [loading, setLoading] = useState(true)
 
-    const createUser = (email, password) => {
-        setLoading(true)
-        return createUserWithEmailAndPassword(auth, email, password)
-    }
+//     const createUser = (email, password) => {
+//         setLoading(true)
+//         return createUserWithEmailAndPassword(auth, email, password)
+//     }
 
-    const signIn = (email, password) => {
-        setLoading(true)
-        return signInWithEmailAndPassword(auth, email, password)
-    }
+//     const signIn = (email, password) => {
+//         setLoading(true)
+//         return signInWithEmailAndPassword(auth, email, password)
+//     }
 
-    const logOut = () => {
-        return signOut(auth);
-    }
+//     const logOut = () => {
+//         return signOut(auth);
+//     }
 
-    useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, currentUser => {
-            setUser(currentUser)
-            setLoading(false)
-        })
-        return () => {
-            unsubscribe();
-        }
+//     useEffect(() => {
+//         const unsubscribe = onAuthStateChanged(auth, currentUser => {
+//             setUser(currentUser)
+//             setLoading(false)
+//         })
+//         return () => {
+//             unsubscribe();
+//         }
 
-    }, [])
-    const authInfo = {
-        user,
-        createUser,
-        signIn,
-        logOut,
-        loading
-    }
+//     }, [])
+//     const authInfo = {
+//         user,
+//         createUser,
+//         signIn,
+//         logOut,
+//         loading
+//     }
 
-    return (
+//     return (
 
-        <AuthContext.Provider value={authInfo}>
-            {children}
-        </AuthContext.Provider>
+//         <AuthContext.Provider value={authInfo}>
+//             {children}
+//         </AuthContext.Provider>
 
-    );
-};
+//     );
+// };
 
 
-export default AuthProvider;
+// export default AuthProvider;
